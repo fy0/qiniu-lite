4c4eb5ae00e06ef07f43525ac0238672febd83e0
fy <fy0748@gmail.com>
2015-03-06 00:06:45 +0800

    修复一点小问题

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 def description():
     return """A lightweight Qiniu Python SDK.
         github: https://github.com/fy0/qiniu-lite
         """
 
+def long_desc():
+    try:
+        return open("demo/tornado.py", 'r').read()
+    except:
+        return description()
+
 setup(name='qiniu-lite',
       version='1.0',
       license = 'BSD',
       description=description(),
+      long_description=long_desc(),
       author = 'fy',
       author_email = 'fy0@qq.com',
       install_requires = ['requests'],
